672b4063960a68e9e53f6fca4929bf2d5bb9ac79
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 05:05:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,6 +17,8 @@ class PigLatinizer
         word[1..-1]+word[0]+"ay"
       elsif !word[0].match(/[aAeEiIoOuU]/) && !word[1].match(/[aAeEiIoOuU]/)
         word[2..-1]+word[0..1]+"ay"
+      elsif !word[0..2].match(/[aAeEiIoOuU]/)
+        word[2..-1]+word[0..1]+"ay"
       end
     end.join(" ")
   end
