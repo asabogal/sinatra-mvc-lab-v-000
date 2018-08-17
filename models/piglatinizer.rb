491878b92cff95026cb5e10fcb2a6215215d4103
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 02:34:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,14 @@ class PigLatinizer
 
   def piglatinize(sentence)
     words_arr = sentence.split(" ")
+
     binding.pry
+    #Condition 1
+    #For words that begin with consonant sounds,
+    #all letters before the initial vowel are placed at the end of the word sequence.
+    #Then, "ay" is added
+
+
   end
 
 
