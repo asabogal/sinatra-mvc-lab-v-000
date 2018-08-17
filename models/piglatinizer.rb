a3ce9f3ab2eb3d0f914bf2476b1243f870f8f7db
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 01:54:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,12 @@
 require 'pry'
 class PigLatinizer
 
-  def piglatinize(sentence)
-    words_arr = sentence.split(" ")
-    binding.pry
-  end
+  # def piglatinize(sentence)
+  #   words_arr = sentence.split(" ")
+  #   binding.pry
+  # end
 
+  def piglatinize(word)
+    word.reverse
+  end
 end
