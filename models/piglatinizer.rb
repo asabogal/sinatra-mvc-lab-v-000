74f89d087510fb7dd4485c5435a95f4e81126f31
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 03:29:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,6 @@ def consonant?(char)
     # word starts with vowel
     if !consonant?(word[0])
       word = word + "w"
-      binding.pry
     # word starts with 3 consonants
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
@@ -18,6 +17,7 @@ def consonant?(char)
     else
       word = word.slice(1..-1) + word.slice(0)
     end
+    binding.pry
     word << "ay"
   end
 
