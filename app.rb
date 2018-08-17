fd8cbb895cacd8a95b47d6cc9a82d9ede4798446
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 01:48:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do
+  post '/' do
     instance = PigLatinizer.new
     @result = instance.piglatinize(params[:word])
     erb :results
