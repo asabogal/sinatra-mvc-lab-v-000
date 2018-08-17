61462d4ad0ef3c3d66ed87e1d78b971e6c1058ab
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 03:33:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,10 +13,12 @@ class PigLatinizer
   end
 
   def piglatinize_word(word)
+
+  binding.pry
     # word starts with vowel
     if !consonant?(word[0])
       word = word + "w"
-      binding.pry
+
     # word starts with 3 consonants
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
