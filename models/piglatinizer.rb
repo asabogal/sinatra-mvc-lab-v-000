f09b593177228edb9e6ac3225bce8edb4d804bd9
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 03:01:33 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,6 +12,7 @@ class PigLatinizer
   end
 
   def piglatinize_word(word)
+    "this is one word"
     #Condition 1
     #For words that begin with consonant sounds,
     #all letters before the initial vowel are placed at the end of the word sequence.
