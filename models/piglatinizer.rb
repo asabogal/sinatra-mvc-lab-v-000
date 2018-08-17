58f2d7044a1465e4fa5ab1e4a762c8e03f004df6
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 04:38:04 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,34 +12,14 @@ class PigLatinizer
   def piglatinize_word(input)
     w = input.split(" ")
     w.collect do |word|
-      #Condition 3
-      #For words that begin with vowel sounds,
-      #one just adds "way" or "yay" to the end (or just "ay")
-      if word[0].match(/[aAeEiIoOuU]/)
+      if word[0].match(/![aAeEiIoOuU]/)
         word+"way"
       elsif !word[0].match(/[aAeEiIoOuU]/) && word[1].match(/[aAeEiIoOuU]/)
         word[1..-1]+word[0]+"ay"
       elsif !word[0].match(/[aAeEiIoOuU]/) && !word[1].match(/[aAeEiIoOuU]/)
         word[2..-1]+word[0..1]+"ay"
       end
-
     end
-    binding.pry
-    # binding.pry
-    #Condition 1
-    #For words that begin with consonant sounds,
-    #all letters before the initial vowel are placed at the end of the word sequence.
-    #Then, "ay" is added
-
-
-
-    #Condition 2
-    #When words begin with consonant clusters (multiple consonants that form one sound),
-    #the whole sound is added to the end when speaking or writing.
-
-
-
-
   end
 
   def piglatinize_sentence(sentence)
