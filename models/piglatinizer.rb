b33868937e138bd4d0b4bc5f4a8be2fb794d73d5
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 02:46:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,15 +6,27 @@ class PigLatinizer
     words_arr.lenght == ? piglatinize_word(input) : piglatinize_sentence(input)
 
     binding.pry
-    #Condition 1
-    #For words that begin with consonant sounds,
-    #all letters before the initial vowel are placed at the end of the word sequence.
-    #Then, "ay" is added
+
 
 
   end
 
   def piglatinize_word(word)
+    #Condition 1
+    #For words that begin with consonant sounds,
+    #all letters before the initial vowel are placed at the end of the word sequence.
+    #Then, "ay" is added
+
+    if word[0]
+
+    #Condition 2
+    #When words begin with consonant clusters (multiple consonants that form one sound),
+    #the whole sound is added to the end when speaking or writing.
+
+    #Condition 3
+    #For words that begin with vowel sounds,
+    #one just adds "way" or "yay" to the end (or just "ay")
+
   end
 
   def piglatinize_sentence(sentence)
